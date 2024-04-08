b5c42c6e7232c1bd67b518342332fb20add1ecca
aumerov <89824787+aumerov@users.noreply.github.com>
2024-04-07 23:20:43 -0700

    env fixed, ready for SQL

---
+++ src/database.py
@@ -0,0 +1,14 @@
+import os
+import dotenv
+from sqlalchemy import create_engine
+
+def database_connection_url():
+    dotenv.load_dotenv()
+
+    url = os.environ.get("POSTGRES_URI")
+    if url is None:
+        raise ValueError("POSTGRES_URI environment variable is not set")
+    return url
+
+# print(database_connection_url()) # debug
+engine = create_engine(database_connection_url(), pool_pre_ping=True)
