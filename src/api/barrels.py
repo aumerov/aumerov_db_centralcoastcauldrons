7a8723d3cc2b1b2a057ebb4f9dfdc0f9fd29b6d4
aumerov <89824787+aumerov@users.noreply.github.com>
2024-04-08 20:14:39 -0700

    feat: added deliver logic

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -20,20 +20,69 @@ class Barrel(BaseModel):
     quantity: int
 
 @router.post("/deliver/{order_id}")
-def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
+def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):  # hard coded to 1 small green barrel for now
     """ """
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
-    
+    with db.engine.begin() as connection: # SELECT step may be unnecessary
+        sql_to_execute = \
+            """SELECT * 
+            FROM global_inventory
+            
+            """
+        result = connection.execute(sqlalchemy.text(sql_to_execute))
+
+        # hard coding for now
+        data = result.fetchall() 
+        num_green_potions = data[0][0]
+        num_green_ml = data[0][1]
+        gold = data[0][2]
+
+        # parse order
+        for barrel in barrels_delivered:
+                if barrel.sku == "SMALL_GREEN_BARREL":
+                    quant = barrel.quantity
+                    price = barrel.price
+                    ml = barrel.ml_per_barrel
+
+        if int(price) * int(quant) > gold:
+             print("Insufficient gold to complete order!")
+             return "Insufficient gold to complete order"
+
+        # else UPDATE
+        sql_to_execute = \
+            f"""UPDATE global_inventory
+            SET num_green_ml = {num_green_ml + ml},
+            gold = {gold - int(quant) * int(price)};
+            """
+        result = connection.execute(sqlalchemy.text(sql_to_execute))
+
+        # print(result.fetchall)
+
+        # check updated table
+        sql_to_execute = \
+            """SELECT * 
+            FROM global_inventory
+            
+            """
+        result = connection.execute(sqlalchemy.text(sql_to_execute))
+
+        data = result.fetchall() 
+        print("Deliver Result: ", data) 
 
     return "OK"
+
+
+
+
+
 
 # Gets called once a day
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
+    print("CATALOG")
     print(wholesale_catalog)
-    #TODO: create logic statements in sql based on inventory, gold
 
     with db.engine.begin() as connection:
             sql_to_execute = \
@@ -45,29 +94,37 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
     # hard coding for now
     data = result.fetchall() 
-    print("Result: ", data) 
+    print("Plan Result: ", data) 
     num_green_potions = data[0][0]
     num_green_ml = data[0][1]
     gold = data[0][2]
 
-    print("GOLD: ", gold) 
-    for item in wholesale_catalog:
-        print(item)
+    # print("GOLD: ", gold) 
+    # print("CATALOG")
+    # for item in wholesale_catalog:
+    #     print(item)
     
     
     # hard coded, only buy small green barrels
     if num_green_potions < 10: # if less than 10 green potions, buy small green barrels
         for barrel in wholesale_catalog:
             if barrel.sku == "SMALL_GREEN_BARREL":
-                quant = barrel.quantity
-                num_eligible_for_purchase = round(gold / barrel.price)
+                stock = barrel.quantity
+                num_eligible_for_purchase = 0
+                if gold >= barrel.price:
+                    num_eligible_for_purchase = round(gold / barrel.price)  
+                    # if we try to buy more barrels than stock  
+                    if num_eligible_for_purchase > stock:  # better way to do this using ':' but I forgot and don't want to look it up rn
+                        num_eligible_for_purchase = stock
+                print(f"Buying {num_eligible_for_purchase} green barrels")
 
         # buy green barrels
         return {
                 "sku": "SMALL_GREEN_BARREL",
                 "quantity": num_eligible_for_purchase
         }
-
+    
+    # else
     return [
         {
             "sku": "SMALL_RED_BARREL",
